d1af84a22b3e44f265ec1f5725cd052cd01c30bc
DAlba-sudo <d.alba.work@gmail.com>
2021-07-21 11:00:00 -0400

    removed test and settings from tracking

--- test.py
+++
@@ -1,9 +0,0 @@
-from PlaneGen import PlaneGen
-from debug import Debug
-from GA.classes.simulation import Simulation
-from frontend.Vis.WingVis import WingVis
-
-db = Debug()
-vis = WingVis()
-wing = PlaneGen.createWing()
-vis.visualize(wing, [])
