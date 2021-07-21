9364cb86d532639f2509abc7c557314705fa883a
DAlba-sudo <d.alba.work@gmail.com>
2021-07-21 13:48:36 -0400

    planegen converted to number representation

--- GA/classes/simulation.py
+++ GA/classes/simulation.py
@@ -3,7 +3,7 @@ from frontend.Vis.WingVis import WingVis
 from debug import Debug
 import numpy
 from PlaneGen import PlaneGen
-from random import choice, paretovariate, random, randrange
+from random import choice, random, randrange
 from .agent import Agent
 from math import floor
 from settings import POPULATION_SIZE, DEATH_THRESH
--- PlaneGen.py
+++ PlaneGen.py
@@ -1,3 +1,5 @@
+from numpy.lib.index_tricks import nd_grid
+from debug import Debug
 from tkinter.constants import X
 from typing import Tuple
 import numpy as np
@@ -36,50 +38,71 @@ class PlaneGen():
         
         # validating that block has neighbors
         if r+1 < len(wing):                         # if within bottom bound
-            hasBot = wing[r+1][c] == 1
-        if r-1 >= 0 and not hasTop:                 # if within top bound
-            hasTop = wing[r-1][c] == 1
-        if c-1 >= 0 and (not hasTop or not hasBot): # if within left bound
-            hasLeft = wing[r][c-1] == 1     
+            hasBot = (wing[r+1][c] == 1)
+        if r-1 >= 0:                 # if within top bound
+            hasTop = (wing[r-1][c] == 1)
+        if c-1 >= 0: # if within left bound
+            hasLeft = (wing[r][c-1] == 1)     
                                                     # if within right bound
-        if c+1 < len(wing[0]) and (not hasTop or not hasBot or not hasLeft):
-            hasRight = wing[r][c+1] == 1
+        if c+1 < len(wing[0]):
+            hasRight = (wing[r][c+1] == 1)
 
         return hasTop or hasBot or hasRight or hasLeft
 
-    # randomly fills said array
-    def __randomFill() -> Tuple[np.ndarray, int]:
+    # generates a random wing represented as a one-dimensional 
+    # binary number
+    def generate(rows:int, cols:int):
+        return random.randrange(0, 2**(rows*cols))
+
+    # generates a matrix from the binary number (provides compatability with matrix operations)
+    def converFromNum(num:int, rows:int, cols:int) -> Tuple[np.ndarray, int]:
         # vars
-        emptyArr = PlaneGen.__createEmpty()     # wing represented as a matrix
-        placed = 0                              # keeps track of the number of blocks placed
+        wing: np.ndarray = np.zeros((rows, cols), dtype=int)
+        binaryWing:str = bin(num)[2:]        # creates the binary string
+        binaryWing = "".rjust(rows*cols - len(binaryWing), "0") + binaryWing
 
-        # random placement loop
-        for r in range(len(emptyArr)):
-            for c in range(len(emptyArr[r])):
-                # places genesis block regardless of neighbors
-                if placed < 9:
-                    emptyArr[r][c] = 1
-                    placed += 1
+        Debug.print(num)
+        Debug.print(binaryWing)
+        Debug.print(f"binary len -> {len(binaryWing)}")
 
-                # 'great filer' (i.e., block is spawned if above threshold)
-                # if has any neighbors, place
-                if PlaneGen.hasNeighbor(emptyArr, r, c) and random.random() >= PlaneGen.SPAWN_RATE_BLOCK: 
-                    emptyArr[r][c] = 1
-                    placed += 1
+        # fills the wing
+        for i in range(rows*cols-1):
+            if binaryWing[i] == "1":
+                r = int((i/cols))
+                c = int(i - r*cols)
+
+                Debug.print(f"1 found in index {i} or ({c}, {r})")
+
+                wing[r][c] = 1
         
-        return emptyArr, placed
+        return PlaneGen.__filterWing(wing)
     
     # crops the wing matrix
-    def __filterWing(wing: np.ndarray):
-        return wing
+    def __filterWing(wing: np.ndarray) -> Tuple[np.ndarray, int]:
+        # vars
+        placed = 0
+        newWing = np.zeros((len(wing), len(wing[0])))
 
+        # checks how many valid blocks are there and removes
+        # blocks without neighbors.
+        for r in range(len(wing)):
+            for c in range(len(wing[0])):
+                if PlaneGen.hasNeighbor(wing, r, c) and wing[r][c] == 1:
+                    placed += 1
+                    newWing[r][c] = 1
+                else:
+                    newWing[r][c] = 0
+
+        return newWing, placed
+
+    # provides compatibility with 1D binary string operations
     def toNumber(wing: np.ndarray):
         # vars
         wingOneD = PlaneGen.__convert(wing)
         num = 0 # number representation of the wing
         last_index = len(wingOneD)-1 # represents the index at the tail of the array
 
-        # scan from behind
+        # scan from behind and convert from binary
         for i in range(len(wingOneD)):
             if wingOneD[last_index-i] == 1:
                 num += 2**i
@@ -88,6 +111,7 @@ class PlaneGen():
 
     # util method for printing wings
     def printWing(wing: np.ndarray):
+        Debug.print(wing)
         for i in wing:
             if 1 in i:
                 print("")
@@ -102,19 +126,12 @@ class PlaneGen():
     # public function for creating wings
     def createWing() -> np.ndarray:
         # init wing with proper num of placed blocks
-        wing, placed = PlaneGen.__randomFill()
-        while not placed >= MINIMUM_BLOCKS_IN_WING:
-            wing, placed = PlaneGen.__randomFill()
-        # filter wing to reduce size
-        return PlaneGen.__filterWing(wing)
+        wing_num = PlaneGen.generate(X_MAX, Y_MAX)
+        wing, placed = PlaneGen.converFromNum(wing_num, rows=Y_MAX, cols=X_MAX)
 
-    # def cropWing(wing: np.ndarray):
-    #     #matrix of matricies
-    #     columns = {}
-    #     filtered_matrix = []
-    #     for i in range(len(wing)):
-    #         for j in range(len(wing[i])):
-    #             columns[str(i).format(i)].append(wing[i])
-    #     for k in range(len(columns)):
-    #         if columns[k] != 0:
-    #             filtered_matrix.append(columns[k])
+        while not placed >= MINIMUM_BLOCKS_IN_WING:
+            wing_num = PlaneGen.generate(X_MAX, Y_MAX)
+            wing, placed = PlaneGen.converFromNum(wing_num, rows=Y_MAX, cols=X_MAX)        # filter wing to reduce size
+        return wing
+        
+        
